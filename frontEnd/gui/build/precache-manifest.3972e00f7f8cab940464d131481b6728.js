201923d211af2917e3d5eb83b8c54aa061fbb2ca
Sathya Narayanan C <mail2sathyanc@yahoo.com>
2019-01-07 08:07:37 +0530

    Add files via upload

---
+++ frontEnd/gui/build/precache-manifest.3972e00f7f8cab940464d131481b6728.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c47ad29474da566b37a6",
+    "url": "/static/js/main.c47ad294.chunk.js"
+  },
+  {
+    "revision": "46f9000118d68969912b",
+    "url": "/static/js/1.46f90001.chunk.js"
+  },
+  {
+    "revision": "c47ad29474da566b37a6",
+    "url": "/static/css/main.29ab131a.chunk.css"
+  },
+  {
+    "revision": "fafc612fd69e63b1a4ce6cd09eaacb71",
+    "url": "/index.html"
+  }
+];
